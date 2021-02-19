b8d7c71d37a0b1d77a6de110e9b677f6a9436975
matari ganan <matari.dev@gmail.com>
2021-02-19 15:20:08 -0800

    Working With Modules

---
+++ app.js
@@ -0,0 +1,8 @@
+const tutorial = require('./tutorial');
+
+console.clear();
+
+console.log(tutorial.sum(3,4));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObj());
+
---
+++ tutorial.js
@@ -0,0 +1,14 @@
+const sum = (num1, num2) => num1 + num2;
+const PI = 3.14;
+class SomeMathObject {
+	constructor() {
+		console.log("object kree-8'd");
+	}
+}
+
+
+module.exports = {
+	sum: sum,
+	PI: PI,
+	SomeMathObj: SomeMathObject
+}
